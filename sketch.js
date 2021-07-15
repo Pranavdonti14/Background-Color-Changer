afaeab5bd09b04b3e2ad684744c73d0ea75a896b
Pranavdonti14 <87311187+Pranavdonti14@users.noreply.github.com>
2021-07-15 23:28:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{ 
+
+ 
+if (keyIsDown(DOWN_ARROW))
+background('limegreen')
+if(keyIsDown(UP_ARROW))
+background('purple')
+if(keyIsDown(LEFT_ARROW))
+background('red')
+if(keyIsDown(RIGHT_ARROW))
+background('blue')
+
+}
+
+
+
+
